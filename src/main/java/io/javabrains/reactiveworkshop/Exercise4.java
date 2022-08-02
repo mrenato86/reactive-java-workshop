9101c373c0bae0ad1538524941e91feac8fa7663
Renato <m.renato@freemail.hu>
2022-08-02 15:45:57 +0200

    Exercise 3, 4

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,21 +1,25 @@
 package io.javabrains.reactiveworkshop;
 
-import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         // Use ReactiveSources.intNumberMono()
 
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(System.out::println);
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        Integer value = ReactiveSources.intNumberMono().block();
+        System.out.println("Value: " + value);
 
-        System.out.println("Press a key to end");
-        System.in.read();
+        Optional<Integer> optionalValue = ReactiveSources.intNumberMono().blockOptional();
+        System.out.println("Optional Value: " + optionalValue);
+
+//        System.out.println("Press a key to end");
+//        System.in.read();
     }
 
 }
