15330e242e3770d7cec4d5366a805bcad9e45df4
Renato <m.renato@freemail.hu>
2022-08-09 01:03:14 +0200

    Exercise 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,18 +1,41 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(
+                System.out::println,
+                err -> System.out.println(err.getMessage()),
+                () -> System.out.println("Complete")
+        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        BaseSubscriber<User> baseSubscriber = new BaseSubscriber<>() {
+            @Override
+            protected void hookOnNext(User value) {
+                System.out.println(value);
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                System.out.println("Complete");
+            }
+        };
+
+        ReactiveSources.userFlux().subscribe(baseSubscriber);
 
         System.out.println("Press a key to end");
         System.in.read();
