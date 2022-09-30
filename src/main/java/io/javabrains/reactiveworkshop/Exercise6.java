887e394a202db83325bfa0857ed0066785ea170f
Renato <m.renato@freemail.hu>
2022-09-30 02:34:38 +0200

    Exercise 6 update

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -24,16 +24,16 @@ public class Exercise6 {
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        List<String> results = ReactiveSources.unresponsiveFlux()
+
+        //List<String> results = ReactiveSources.unresponsiveFlux().collectList().block(Duration.ofSeconds(5));
+        List<String> results_alt = ReactiveSources.unresponsiveFlux()
                 .timeout(Duration.ofSeconds(5))
                 .doOnError(e -> System.out.println("No value from unresponsiveFlux within 5 seconds"))
                 .onErrorReturn("n/a")
-//                .collectList()
-//                .block();
                 .toStream()
                 .toList();
 
-        System.out.println(results);
+        System.out.println(results_alt);
 
 //        System.out.println("Press a key to end");
 //        System.in.read();
